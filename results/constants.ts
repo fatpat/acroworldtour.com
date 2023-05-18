3da28f44007133164cbd249e9ce536da90417fb7
Pierre-François Salmand <maabaa@gmail.com>
2023-05-19 02:54:39 +0700

    [results]Switch to backup API domain

--- results/constants.ts
+++ results/constants.ts
@@ -1,4 +1,5 @@
 export const SPORTING_CODE_MANUAL_URL =
   "https://www.fai.org/sites/default/files/civl/documents/sporting_code_s7_b_-_aerobatics_2023.pdf";
 
-export const API_URL = "https://api.acroworldtour.com";
+// export const API_URL = "https://api.acroworldtour.com";
+export const API_URL = "https://api-acroworldtour-com.onrender.com";
