3626d93e1b430263ce6a34b17a554fd7fc330dd8
Pierre-François Salmand <maabaa@gmail.com>
2023-05-19 00:26:09 +0700

    [results]Update API to prod

--- results/pages/pilots/index.tsx
+++ results/pages/pilots/index.tsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import PilotCard from "@/components/pilot/pilotCard";
 import FetchError from "@/components/ui/fetchError";
 import FetchLoading from "@/components/ui/fetchLoading";
+import { API_URL } from "@/constants";
 import { components } from "@/types";
 import { fetcher } from "@/utils/fetcher";
 
@@ -13,7 +14,7 @@ const pageDescription = "All the pilots of the Acro World Tour";
 
 const Pilots = () => {
   const { data: pilots, error } = useSWR<Pilot[], Error>(
-    "https://api-preprod.acroworldtour.com/public/pilots",
+    `${API_URL}/public/pilots`,
     fetcher
   );
 
