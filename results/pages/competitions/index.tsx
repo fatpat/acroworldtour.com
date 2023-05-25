26f3564e9b7364b489744936f85a294bd8748327
Pierre-François Salmand <maabaa@gmail.com>
2023-05-25 14:12:35 +0700

    [results]Implement year selector in competitions index

--- results/pages/competitions/index.tsx
+++ results/pages/competitions/index.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 import CompetitionCard from "@/components/competition/competitionCard";
@@ -13,7 +13,15 @@ import { fetcher } from "@/utils/fetcher";
 type Competition = components["schemas"]["CompetitionPublicExport"];
 type Season = components["schemas"]["SeasonExport"];
 
+interface YearSelectorProps {
+  years: number[];
+  // eslint-disable-next-line no-unused-vars
+  onChange: (year: number) => void;
+}
+
 const Competitions = () => {
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
+
   const {
     setPageTitle,
     setPageDescription,
@@ -48,17 +56,79 @@ const Competitions = () => {
   if (competitionsError || seasonsError) return <FetchError />;
   if (!competitions || !seasons) return <FetchLoading />;
 
+  const filteredCompetitions = selectedYear
+    ? competitions.filter((competition) => {
+        const startYear = new Date(competition.start_date).getFullYear();
+        const endYear = new Date(competition.end_date).getFullYear();
+        return startYear === selectedYear || endYear === selectedYear;
+      })
+    : competitions;
+
   const soloSeasons = seasons.filter(
-    (season) => season.type === "solo" && season.competitions.length > 0
+    (season) =>
+      season.type === "solo" &&
+      season.competitions.some((comp) =>
+        filteredCompetitions.some(
+          (filteredComp) => filteredComp.code === comp.code
+        )
+      )
   );
 
-  const offSeasonCompetitions = competitions.filter(
+  const offSeasonCompetitions = filteredCompetitions.filter(
     (competition) => competition.seasons.length === 0
   );
 
+  const YearSelector: React.FC<YearSelectorProps> = ({ years, onChange }) => {
+    const handleYearChange = ({
+      target,
+    }: React.ChangeEvent<HTMLSelectElement>) => {
+      const year = parseInt(target.value);
+      setSelectedYear(year);
+      onChange(year);
+    };
+
+    return (
+      <div className="mt-4 font-semibold opacity-75">
+        <label htmlFor="year-selector" className="mr-2">
+          Filter by year:
+        </label>
+        <select
+          id="year-selector"
+          className="bg-inherit font-sans"
+          value={selectedYear ?? ""}
+          onChange={handleYearChange}
+        >
+          <option value="" className="text-red-500">
+            All
+          </option>
+          {years.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+
   return (
     <>
-      <h2 className="-mb-6">All Competitions</h2>
+      <YearSelector
+        years={[
+          1970, // TESTING ONLY
+          ...new Set(
+            competitions.map((comp) => new Date(comp.start_date).getFullYear())
+          ),
+        ]}
+        onChange={setSelectedYear}
+      />
+      <h2 className="-mb-6">
+        {selectedYear
+          ? filteredCompetitions.length === 0
+            ? `No competition in ${selectedYear}`
+            : `Competitions in ${selectedYear}`
+          : "All competitions"}
+      </h2>
       {soloSeasons
         .sort((a, b) => b.year - a.year || b.code.localeCompare(a.code))
         .map((season) => {
@@ -77,14 +147,21 @@ const Competitions = () => {
             </>
           );
         })}
-      <h3 className="mb-6 mt-12 opacity-50">Off Season</h3>
-      <section className={classNames("wrapper")}>
-        {offSeasonCompetitions
-          .sort((a, b) => b.start_date.localeCompare(a.start_date))
-          .map((competition) => (
-            <CompetitionCard key={competition.code} competition={competition} />
-          ))}
-      </section>
+      {offSeasonCompetitions.length > 0 && (
+        <>
+          <h3 className="mb-6 mt-12 opacity-50">Off Season</h3>
+          <section className={classNames("wrapper")}>
+            {offSeasonCompetitions
+              .sort((a, b) => b.start_date.localeCompare(a.start_date))
+              .map((competition) => (
+                <CompetitionCard
+                  key={competition.code}
+                  competition={competition}
+                />
+              ))}
+          </section>
+        </>
+      )}
     </>
   );
 };
