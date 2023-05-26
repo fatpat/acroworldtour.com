b1c1ee3f7556df988c46a2d998ae87e80c226771
Pierre-François Salmand <maabaa@gmail.com>
2023-05-26 10:25:01 +0700

    [results]Revert to next config for security headers

--- results/next.config.js
+++ results/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["api.acroworldtour.com", "civlcomps.org"],
   },
