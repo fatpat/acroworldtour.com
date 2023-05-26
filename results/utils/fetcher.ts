723cb5ebf1ce67d4404c6193fad9366b9ca76cdc
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2023-05-26 12:51:01 +0200

    [results][utils] handles errors on fetcher

--- results/utils/fetcher.ts
+++ results/utils/fetcher.ts
@@ -1,2 +1,5 @@
 export const fetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json());
+  fetch(...args).then((res) => {
+      if (! res.ok) throw new Error(`API returned code ${res.status}`)
+      return res.json()
+  });
