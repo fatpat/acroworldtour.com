98594fcc9d51bc3f56b33fdb841d6c539e970c24
Pierre-François Salmand <maabaa@gmail.com>
2023-06-07 00:00:14 +0700

    [results][theme] Change grey from neutral to slate (cool)

--- results/tailwind.config.js
+++ results/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'awt-dark': colors.neutral,
+        'awt-dark': colors.slate,
         'awt-accent': colors.sky,
       },
     },
